7115424c90e2d7f64b06bafe1abcde3235e3072b
non0921 <taruto7771@gmail.com>
2026-06-17 20:31:12 +0900

    feat: shopモデルにfillable,リレーションを追加

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Shop extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'address',
+        'description',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function reservations(): HasMany
+    {
+        return $this->hasMany(Reservation::class);
+    }
+
+    
 }
