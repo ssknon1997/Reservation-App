ee773daff54dab7f6ce8a4cb94780ff49fb36e92
non0921 <taruto7771@gmail.com>
2026-06-17 17:52:28 +0900

    feat: shopsテーブルのmigrationを追加

--- database/migrations/2026_06_17_081956_create_shops_table.php
+++ database/migrations/2026_06_17_081956_create_shops_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('shops', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('name');
+            $table->string('address');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
