c9f0ee059c357aacfd2b47069aa4aab4d48d3916
non0921 <taruto7771@gmail.com>
2026-06-19 01:56:40 +0900

    feat: 予約のルーティングを追加

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ShopController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\OwnerReservationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,8 +15,14 @@ Route::middleware(['auth'])->group(function () {
     Route::middleware(['owner'])->group(function () {
         Route::resource('shops', ShopController::class)
         ->only(['create', 'store', 'edit', 'update', 'destroy']);
+
+        Route::get('owner/reservations', [OwnerReservationController::class, 'index'])
+            ->name('owner.reservations.index');
+        Route::get('owner/reservations/{reservation}', [OwnerReservationController::class, 'show'])
+            ->name('owner.reservations.show');
     });
-    
+
+    Route::resource('reservations', ReservationController::class);
 });
 
 Route::resource('shops', ShopController::class)
