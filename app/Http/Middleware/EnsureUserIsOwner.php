0f41cdb73d673a1b059712139f308dda2c427daa
non0921 <taruto7771@gmail.com>
2026-06-19 12:06:38 +0900

    feat: オーナー以外を弾くMiddlewareを追加

---
+++ app/Http/Middleware/EnsureUserIsOwner.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureUserIsOwner
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(! $request->user() || ! $request->user()->isOwner() ) {
+            abort(403, 'オーナーのみアクセスできます');
+        }
+        return $next($request);
+    }
+}
