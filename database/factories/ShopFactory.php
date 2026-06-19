a26d23dbdc3ae9d938a42bfac3e823672f28a74a
non0921 <taruto7771@gmail.com>
2026-06-19 12:43:50 +0900

    feat: 店舗のFactoryを追加

---
+++ database/factories/ShopFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Shop;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Shop>
+ */
+class ShopFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'name' => fake()->company(),
+            'address' => fake()->address(),
+            'description' => fake()->text(200),
+        ];
+    }
+}
