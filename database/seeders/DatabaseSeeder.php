0b3c0dbc72c88fa073450a0dbe45b8a10c3cb61c
non0921 <taruto7771@gmail.com>
2026-06-20 02:03:21 +0900

    feat: seederにデータを追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Shop;
+use App\Models\Reservation;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,11 +17,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $users = User::factory(50)->create([
+            'role' => 'user',
         ]);
+
+        $owners = User::factory(4)->create([
+            'role' => 'owner',
+        ]);
+
+        $shops = Shop::factory(12)->recycle()->create([
+            'user_id' => $owners->random()->id,
+        ]);
+
+        Reservation::factory(100)->recycle()->create([
+            'user_id' => $users->random()->id,
+            'shop_id' => $shops->random()->id,
+        ]);
+
     }
 }
