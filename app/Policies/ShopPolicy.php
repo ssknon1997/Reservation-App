d24841aebca37e1d27d6bd9ac9d906d40dc81065
non0921 <taruto7771@gmail.com>
2026-06-21 02:32:00 +0900

    fix: タイプミスを修正

---
+++ app/Policies/ShopPolicy.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Shop;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class ShopPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, Shop $shop): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return $user->isOwner();
+    }
+
+    public function update(User $user, Shop $shop): bool
+    {
+        return $user->isOwner() && $user->id === $shop->user_id;
+    }
+
+    public function delete(User $user, Shop $shop): bool
+    {
+        return $user->isOwner() && $user->id === $shop->user_id;
+    }
+
+}
