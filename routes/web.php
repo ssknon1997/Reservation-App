75b43980df16f59f4768b09f324b5bd98fcb98d1
non0921 <taruto7771@gmail.com>
2026-06-22 02:35:03 +0900

    feat: navigationのルーティングを追加

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\OwnerReservationController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -27,5 +28,16 @@ Route::middleware(['auth'])->group(function () {
 
 Route::resource('shops', ShopController::class)
     ->only(['index', 'show']);
+
+Route::get('/dashboard', function () {
+    return redirect()->route('shops.index');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 require __DIR__.'/auth.php';
