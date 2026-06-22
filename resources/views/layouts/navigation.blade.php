b8f669b696a7b444350687fadb6695c14cdfa6af
non0921 <taruto7771@gmail.com>
2026-06-22 15:00:37 +0900

    fix: ロゴを削除

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,14 +2,7 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('shops.index') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                    </a>
-                </div>
 
-                <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('shops.index')" :active="request()->routeIs('shops.index')">
                         {{ __('店舗一覧') }}
@@ -31,7 +24,6 @@
                 </div>
             </div>
 
-            <!-- 右側：ログイン状態で分岐 -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 @auth
                     <x-dropdown align="right" width="48">
@@ -62,7 +54,6 @@
                         </x-slot>
                     </x-dropdown>
                 @else
-                    {{-- ゲスト（未ログイン）の場合 --}}
                     <a href="{{ route('login') }}"
                         class="text-sm text-gray-600 hover:text-gray-900 mr-4">
                         ログイン
@@ -74,7 +65,6 @@
                 @endauth
             </div>
 
-            <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
@@ -86,7 +76,6 @@
         </div>
     </div>
 
-    <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('shops.index')" :active="request()->routeIs('shops.index')">
