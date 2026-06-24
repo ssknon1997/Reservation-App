35f8480be4da2d66292f90cf1ee42e9934926d69
non0921 <taruto7771@gmail.com>
2026-06-25 01:32:50 +0900

    fix: カーソルを乗せた時の色を修正

--- resources/views/reservations/show.blade.php
+++ resources/views/reservations/show.blade.php
@@ -45,7 +45,7 @@
                         onsubmit="return confirm('削除しますか？')">
                         @csrf
                         @method('DELETE')
-                        <button class="text-red-500 hover:underline">削除</button>
+                        <button class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600">削除</button>
                     </form>
                 @endif
             </div>
